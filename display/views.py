67465d1addc76eb45acd78b9fda38f0f76c1643d
ramza007 <ramza007@gmail.com>
2018-05-04 16:56:52 +0300

    started project work

--- display/views.py
+++ display/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+import datetime as dt
+
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Something Great Is Coming!')
