4ace03a268863cbd1288cd109e60266218595caf
ramza007 <ramza007@gmail.com>
2018-05-05 14:40:50 +0300

    added styles

--- display/models.py
+++ display/models.py
@@ -8,3 +8,13 @@ class Images(models.Model):
     photo = models.ImageField(upload_to='Images/', null=True)
     name = models.CharField(max_length=30)
     descripton = models.TextField()
+
+
+class Location(models.Model):
+    """ class to indicate where the image was taken"""
+    name = models.CharField(max_length=30)
+
+
+class Tag(models.Model):
+    """ class to indicate the category of the image"""
+    name = models.CharField(max_length=30)
