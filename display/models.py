3d9530181677bb7bf5d22a1d587387d96aa5f3b9
ramza007 <ramza007@gmail.com>
2018-05-06 12:50:24 +0300

    added to navbar

--- display/models.py
+++ display/models.py
@@ -1,20 +1,21 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
-
-
-class Images(models.Model):
-    """class to hold the photos"""
-    photo = models.ImageField(upload_to='Images/', null=True)
-    name = models.CharField(max_length=30)
-    descripton = models.TextField()
 
 
 class Location(models.Model):
     """ class to indicate where the image was taken"""
     name = models.CharField(max_length=30)
 
+class Images(models.Model):
+    """class to hold the photos"""
+    photo = models.ImageField(upload_to='Images/', null=True)
+    name = models.CharField(max_length=30)
+    descripton = models.TextField()
+    location_taken = models.ForeignKey(Location, on_delete=True)
 
 class Tag(models.Model):
     """ class to indicate the category of the image"""
     name = models.CharField(max_length=30)
+
