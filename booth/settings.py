947374c77bfd0f28984544b355b5d3c6d0d45fab
ramza007 <ramza007@gmail.com>
2018-05-07 12:50:20 +0300

    heroku

--- booth/settings.py
+++ booth/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'booth.urls'
@@ -127,3 +128,5 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+import django_heroku
+django_heroku.settings(locals)
