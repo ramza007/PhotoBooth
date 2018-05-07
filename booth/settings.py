d6dab1635753fe9a434f10faca5cbd5503f7be26
ramza007 <ramza007@gmail.com>
2018-05-07 14:00:42 +0300

    heroku errors

--- booth/settings.py
+++ booth/settings.py
@@ -124,9 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 import django_heroku
-django_heroku.settings(locals)
+django_heroku.settings(locals())
